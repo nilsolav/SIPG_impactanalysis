dc5bb63363bbe73c192a7a1890334da10b447ca5
Nils Olav Handegard <nils.olav.handegard@imr.no>
2022-09-24 11:22:00 +0200

    Added functions to retrieve publicatoins by authors.

--- authorsearch.py
+++ authorsearch.py
@@ -7,17 +7,18 @@ list(df.columns)
 
 # Pick time interval
 df['publisherPublication'] = pd.to_datetime(df['publisherPublication'])
-ind = (df['publisherPublication'] > datetime.datetime(2021, 1, 1)) & (
+ind = (df['publisherPublication'] > datetime.datetime(2001, 1, 1)) & (
     df['publisherPublication'] > datetime.datetime(2022, 1, 1))
 df1 = df[ind]
-
+df1 = df
 # Search for names
-names = ['Handegard', 'Bundy', 'Breivik'] # need to refect what you are searching for
+names = ['Handegard'] # need to refect what you are searching for
 hits = [df1['Contributors (Authors)'].str.find(name) > 0 for name in names]
 hit = hits[0]
 for _hits in hits:
     hit = hit | _hits
 
 # List results
-df1[hit][['Contributors (Authors)','title_x']]
+results = df1[hit][['Contributors (Authors)', 'title_x', 'publisherPublication', 'Series', 'url_public_html_y']]
 
+results.to_csv('/mnt/c/DATAscratch/SIPG/ICESauthoredpublications.csv', sep = ';')
