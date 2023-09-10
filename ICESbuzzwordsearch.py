6387728cf051a06c361dc239208137a1618e06f3
Nils Olav Handegard <nils.olav.handegard@imr.no>
2023-09-10 16:57:49 +0200

    Added more keywords and plotting.

--- ICESbuzzwordsearch.py
+++ ICESbuzzwordsearch.py
@@ -1,8 +1,5 @@
 import matplotlib.pyplot as plt
 import pandas as pd
-from wordcloud import WordCloud
-from nltk.tokenize import word_tokenize
-from nltk.corpus import stopwords
 import os
 
 files = pd.read_pickle('/mnt/c/DATAscratch/SIPG/ICESfiles.pk')
@@ -10,15 +7,25 @@ df = pd.read_pickle('/mnt/c/DATAscratch/SIPG/ICESpublications.pk')
 
 # find subset of files based on decade
 dec = pd.date_range('1889-12-31',
-                    '2019-12-31',
+                    '2023-12-31',
                     freq = '1Y')
 
-ecosystem = []
+keywords = ['fisheries management',
+            'integrated ecosystem assessment',
+            'ecosystem based',
+            'multi species',
+            'recruitment',
+            'north sea',
+            'baltic',
+            'norwegian sea',
+            'aquaculture',
+            'mariculture',
+            'cod', 'herring', 'eel', 'mackerel', 'salmon',
+            'hydrography','oceanography',
+            'plankton']
+
+dat = []
 Y = []
-r1 = []
-r2 = []
-r3 = []
-r4 = []
 N = []
 
 for _dec in dec:
@@ -41,25 +48,39 @@ for _dec in dec:
             _data = pd.read_pickle(pkfile)
             data = pd.concat([data, _data], ignore_index = True)
 
-    # Create wordcloud for this decade
+    # Create text string for _dec
     text = ' '.join(data['keywords'])
-    if len(data) > 0:
-        Y.append(_dec)
-        N.append(text.lower().count(' '))
-        r1.append(text.lower().count('ecosystem '))
-        r2.append(text.lower().count('integrated ecosystem assessment'))
-        r3.append(text.lower().count('ecosystem based'))
-        r4.append(text.lower().count('multi species'))
+    if len(data) == 0:
+        text = ' '
+    
+    Y.append(_dec)
+    N.append(text.lower().count(' '))
 
+    cnt = []
+    for _keywords in keywords:
+        cnt.append(text.lower().count(_keywords))
+    dat.append(cnt)
 
-df = pd.DataFrame(list(zip(N, r1, r2, r3, r4)), index = Y,
-                  columns =['N', 'ecosystem', 'IEA','ecosystem based','multi species'])
-
+# Change to data frame
+df = pd.DataFrame(dat, columns=keywords, index=Y)
+df['N'] = pd.DataFrame(N, index=Y)
 df2 = pd.DataFrame()
-df2['ecosystem'] = df['ecosystem']/df['N']
-df2['Integrated Ecosystem Assessment'] = df['IEA']/df['N']
-df2['ecosystem based'] = df['ecosystem based']/df['N']
-df2['multi species'] = df['multi species']/df['N']
+for _keywords in keywords:
+    df2[_keywords] = df[_keywords]/df['N']
 
-df2.plot()
+# Aquaculture in ICES?
+f3 = df2[['mariculture', 'aquaculture']].plot()
+
+# Areas?
+f4 = df2[['north sea', 'baltic', 'norwegian sea']].plot()
+
+f5 = df2[['fisheries management',
+          'integrated ecosystem assessment',
+          'ecosystem based',
+          'multi species']].plot()
+
+f6 = df2[['cod', 'herring', 'eel', 'mackerel', 'salmon']].plot()
+
+f7 = df2[['hydrography', 'oceanography', 'plankton']].plot()
+
 plt.show()
